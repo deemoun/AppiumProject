8bed006d202f85c6234d712b9f03ac820b6445fd
Dmitry Y. <deemoun@users.noreply.github.com>
2023-11-27 10:15:55 +0100

    Add new tests

--- src/main/java/org/deemoun/AppiumTest.java
+++ src/main/java/org/deemoun/AppiumTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import static org.junit.Assert.assertEquals;
 
 public class AppiumTest {
 
@@ -19,17 +20,30 @@ public class AppiumTest {
     public static void setUp() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("deviceName", "emulator-5554"); // Replace with your device name
-        caps.setCapability("app", "/Users/deemounus/Desktop/ApiDemos-debug.apk"); // Replace with the path to your app APK
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("app", "/Users/deemounus/Desktop/ApiDemos-debug.apk");
 
         URL appiumServerURL = new URL("http://localhost:4723/wd/hub");
         driver = new AndroidDriver(appiumServerURL, caps);
     }
 
+//    @Test
+//    public void checkNumber(){
+//        int a = 5;
+//        int b = 5;
+//        int c = a + b;
+//        assertEquals(10, c);
+//    }
     @Test
     public void pressTextButton() {
         // Test Logic Goes Here
-    driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Text\"]")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Text\"]")).click();
+        driver.navigate().back();
+    }
+
+    @Test
+    public void pressAnimationButton(){
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Animation\"]"));
     }
 
     @AfterClass
